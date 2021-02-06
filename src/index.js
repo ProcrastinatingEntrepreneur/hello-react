9acec9c69b2022150f53a9179df7dcdc0ded1464
ProcrastinatingEntrepreneur <benjamin.metcalfe@my.tccd.edu>
2021-02-05 21:34:39 -0600

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Hello({library, message, number}) {
+  return (
+    <div>
+      <h1>Welcome to {library}!</h1>
+      <p>Hello World! {message}</p>
+      <p>{number} Props Total</p>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Hello 
+    library="Vue" 
+    message="have fun" 
+    number={3} 
+  />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
